c25da4c529bfa30da07d3a468d1fcb8120883323
Taheem Akter <cyberpy0@gmail.com>
2024-07-07 15:24:48 +0000

    Added speed analytics for Vercel.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import PrayerTimes from './PrayerTimes'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function App() {
 	return (
 	<>
 		<PrayerTimes />
+		<SpeedInsights />
 	</>
 	)
 }
